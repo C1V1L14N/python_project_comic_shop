0ba0d7318fede4a85f73e5346ff59b5af11197fa
Kenny <kenny.mckeown@gmail.com>
2020-11-08 10:21:57 +0000

    repositories complete & untested

--- app/repositories/comic_repository.py
+++ app/repositories/comic_repository.py
@@ -9,4 +9,59 @@ def save(comic):
     values = [comic.name, comic.author, comic.genre, comic.wholesale_price,\ 
     comic.markup, comic.stock_count, comic.min_count, comic.out_of_stock, comic.publisher.id]
     results = run_sql(sql, values)
-    comic.id = results[0]['id']
+    id = results[0]['id']
+    comic.id = id
+    return comic
+
+def select_all():
+     comics = []
+
+     sql = "SELECT * FROM comics"
+     results = run_sql(sql)
+
+     for row in results:
+          comic = Comic(row['name'], row ['author'], row['genre'], row['wholesale_price'], row['markup'], row['stock_count'], row['min_count'], row['out_of_stock'], row['publisher'])
+          comics.append(comic)
+     return comics
+
+
+def select():
+     comic = None
+     sql = "SELECT * FROM comics WHERE id = %s"
+     values = [id]
+     result = run_sql(sql, values)
+
+     if result is not None:
+          comic = Comic(result['name'], result ['author'], result['genre'], result['wholesale_price'], result['markup'], result['stock_count'], result['min_count'], result['out_of_stock'], result['publisher'])
+          return comic
+
+
+def delete_all():
+     sql = "DELETE  FROM comics"
+     run_sql(sql)
+
+
+def delete():
+     sql = "DELE  FROM comics WHERE id = %s"
+     values = id
+     run_sql(sql, values)
+
+
+def update(comic):
+     sql = "UPDATE comics SET (name, author, genre, wholesale_price, markup, stock_count, min_count, out_of_stock, publisher) = (%s, %s, %s, %s, %s, %s, %s, %s, %s,) WHERE id = %s"
+     values = [comic.name, comic.author, comic.genre, comic.wholesale_price, comic.markup, comic.stock_count, comic.min_count, comic.out_of_stock, comic.publisher, comic.id]
+     result = run_sql(sql, values)
+
+def publisher(comic):
+     sql = "SELECT * FROM comics WHERE publisher_id = %s"
+     values = [comic.id]
+     result = run_sql(sql, values)
+
+     for row in result:
+          publisher = Publisher(row['name'], row['id'])
+     return publisher
+
+
+
+def publisher(comic):
+
--- app/repositories/publisher_repository.py
+++ app/repositories/publisher_repository.py
@@ -15,17 +15,52 @@ def save(publisher):
 def select_all():
     publishers = []
 
+    sql = "SELECT * FROM publishers"
+    results = run_sql(sql)
+    
+    for row in results:
+        publisher = Publisher(row['publisher_id'], row['id'])        
+        publishers.append(publisher)
+    return publishers
+
 
 def select(id):
+    publisher = None
+    sql = "SELECT * FROM publishers WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        publisher = Publisher(result['name'], result['id'])
+    return publisher
 
 
 def delete_all():
+    sql = "DELETE  FROM publishers"
+    run_sql(sql)
 
 
 def delete(id):
+    sql = "DELETE  FROM publishers WHERE id = %s"
+    values = id
+    run_sql(sql, values)
 
 
 def update(publisher):
+    sql = "UPDATE publishers SET (name) = (%s) WHERE id = %s"
+    values = [publisher.name, publisher.id]
+    run_sql(sql, values)
+
+def comics(publisher):
+    comics = []
+
+    sql = "SELECT * FROM publishers WHERE comic_id = %s"
+    values = [publisher.id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        comic = Comic(row['name'], row ['author'], row['genre'], row['wholesale_price'], row['markup'], row['stock_count'], row['min_count'], row['out_of_stock'], row['publisher'])
+        comics.append(publisher)
 
 
 
